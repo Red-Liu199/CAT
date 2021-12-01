[LM] modified truncate and concat way [LM] eval loss computation bug fixed

cat/lm/train.py
@@ -48,8 +48,9 @@ def main_worker(gpu: int, ngpus_per_node: int, args: argparse.Namespace):
 
     manager = Manager(CorpusDataset, sortedPadCollateLM(),
                       args, build_model, func_eval=evaluate)
-    manager.model.eval()
+
     if args.eval is not None:
+        manager.model.eval()
         ppl = evaluate(manager.valloader, args, manager)
         utils.distprint(f"Perplexity over dataset is {ppl:.2f}", args.gpu)
         return
@@ -78,35 +79,12 @@ class LMTrainer(nn.Module):
 
         # targets: (\sum{S_i})
         loss = self.criterion(logits, targets)
-        return loss
-
-
-class PerplexityLoss(nn.CrossEntropyLoss):
-    def __init__(self, reduction: str = 'mean', *args, **kwargs) -> None:
-        super().__init__(reduction='none', *args, **kwargs)
-        assert reduction in ['mean', 'sum',
-                             'none'], f"unknown reduction: {reduction}"
-        self.ppl_reduction = reduction
-
-    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        gather_loss = super().forward(input, target)
-        gather_ppl = torch.exp(gather_loss)
-        if self.ppl_reduction == 'mean':
-            return gather_ppl.mean()
-        elif self.ppl_reduction == 'sum':
-            return gather_ppl.sum()
-        elif self.ppl_reduction == 'none':
-            return gather_ppl
-        else:
-            raise RuntimeError(
-                f"Invalid reduction option: {self.ppl_reduction}, expected one of ['mean', 'sum', 'none'].")
+        return loss, input_lengths.sum()
 
 
 @torch.no_grad()
-def evaluate(testloader: DataLoader, args: argparse.Namespace, manager: Manager):
-
-    avg_ce_loss = default_eval(testloader, args, manager)
-    return math.exp(avg_ce_loss)
+def evaluate(*args) -> float:
+    return math.exp(default_eval(*args))
 
 
 def build_model(args, configuration, dist=True, wrapper=True) -> LMTrainer:

cat/shared/coreutils.py
@@ -210,8 +210,6 @@ def BasicDDPParser(istraining: bool = True, prog: str = '') -> argparse.Argument
                             help="Manual seed.")
         parser.add_argument("--amp", action="store_true",
                             help="Enable automatic mixed precision training.")
-        parser.add_argument("--update-bn", action="store_true",
-                            help="Update batchnorm stats for model averaging.")
         parser.add_argument("--grad-accum-fold", type=int, default=1,
                             help="Utilize gradient accumulation for K times. Default: K=1")
         parser.add_argument("--grad-norm", type=float, default=0.0,
@@ -391,12 +389,13 @@ def setPath(args: argparse.Namespace):
     if not args.debug and args.resume is None:
         if os.listdir(checksdir) != []:
             raise FileExistsError(
-                f"{args.checksdir} is not empty! Refuse to run.")
+                f"{args.checksdir} is not empty!")
+
         from .monitor import FILE_WRITER
-        logfile = os.path.join(args.logsdir, FILE_WRITER)
+        logfile = os.path.join(logsdir, FILE_WRITER)
         if os.path.isfile(logfile):
             raise FileExistsError(
-                f"{logfile} exist! Refuse to run.")
+                f"{logfile} exists!")
 
 
 def load_checkpoint(model: Union[torch.nn.Module, torch.nn.parallel.DistributedDataParallel], path_ckpt: str) -> torch.nn.Module:

cat/shared/data.py
@@ -204,8 +204,8 @@ class CorpusDataset(AbsDataset):
             self.dataset = open(self._pathbin, 'rb')
 
         self.dataset.seek(self._seeks[index], 0)
-        data = pickle.load(self.dataset)    # type: Sequence[int]
-        return torch.LongTensor(data)
+        x, y = pickle.load(self.dataset)
+        return torch.LongTensor(x), torch.LongTensor(y)
 
 
 class NbestListDataset(AbsDataset):
@@ -353,38 +353,32 @@ class sortedPadCollateLM():
     """Collect data into batch by desending order and add padding.
 
     Args:
-        batch  : list of label
-            label  : torch.LongTensor
+        batch  : [(labels, targets)]
+            labels  : torch.LongTensor
+            targets : torch.LongTensor
 
     Return:
-        (labels_with_bos, label_lengths, labels_with_eos, `torch.empty(1)`)
+        (labels, label_lengths, targets, `torch.empty(1)`)
     """
 
     def __init__(self, flatten_target: bool = True) -> None:
         self.flatten_target = flatten_target
 
-    def __call__(self, batch: Sequence[torch.LongTensor]):
-        batches = [(label, label.size(0)) for label in batch]
+    def __call__(self, batch: Tuple[List[torch.LongTensor], List[torch.LongTensor]]):
+        batch_sorted = sorted(
+            batch, key=lambda item: item[0].size(0), reverse=True)
 
-        batch_sorted = sorted(batches, key=lambda item: item[1], reverse=True)
+        X, Y = list(zip(*batch_sorted))
+        input_lengths = torch.LongTensor(
+            [x.size(0) for x in X])  # type: torch.LongTensor
+        xs = utils.pad_list(X)   # type: torch.Tensor
 
-        xs = utils.pad_list([x[0] for x in batch_sorted]
-                            )   # type: torch.Tensor
-        # xs -> <s> + xs
-        xs = torch.cat([xs.new_zeros(xs.size(0), 1), xs], dim=1)
-
-        # labels -> labels + <s>
-        labels = [torch.cat([x, x.new_zeros(1)]) for x, _ in batch_sorted]
         if self.flatten_target:
-            labels = torch.cat(labels)
+            target = torch.cat(Y, dim=0)
         else:
-            labels = utils.pad_list(labels)
+            target = utils.pad_list(Y)
 
-        input_lengths = torch.LongTensor([l+1 for _, l in batch_sorted])
-
-        label_lengths = torch.empty(1)
-
-        return xs, input_lengths, labels, label_lengths
+        return xs, input_lengths, target, torch.empty(1)
 
 
 class BalancedDistributedSampler(DistributedSampler):

cat/shared/decoder.py
@@ -56,10 +56,9 @@ class AbsDecoder(nn.Module):
             if tied:
                 self.classifier.weight = self.embedding.weight
 
-    def score(self, input_ids: torch.LongTensor, input_lengths: torch.LongTensor, *args):
+    def score(self, input_ids: torch.LongTensor, targets: torch.LongTensor, input_lengths: torch.LongTensor, *args):
         # [N, U, K]
         logits, _ = self.forward(input_ids, input_lengths=input_lengths, *args)
-        targets = input_ids.roll(-1, 1)
         # [N, U]
         log_prob = logits.log_softmax(
             dim=-1).gather(index=targets.unsqueeze(2), dim=-1).squeeze(-1)
@@ -336,29 +335,45 @@ class CausalTransformer(AbsDecoder):
 
 
 class NGram(AbsDecoder):
-    def __init__(self, gram_n: int, num_classes: int, f_binlm: str) -> None:
+    def __init__(self,
+                 gram_order: int,
+                 num_classes: int,
+                 f_binlm: str,
+                 bos_id: int = 0,
+                 eos_id: int = -1,
+                 unk_id: int = 1) -> None:
         super().__init__(n_emb=1, with_head=False)
         del self.embedding
         del self.classifier
-        self.gram_n = gram_n
-        self.vocab = [str(x) for x in range(num_classes)]
+        self.gram_order = gram_order
+        self.vocab = {x: str(x) for x in range(num_classes)}
         # set 0 -> </s>, 1 -> <unk>
-        self.vocab[0] = '</s>'
-        self.vocab[1] = '<unk>'
+        if eos_id == -1:
+            eos_id = bos_id
+        self.vocab[bos_id] = '<s>'
+        self.vocab[eos_id] = '</s>'
+        self.vocab[unk_id] = '<unk>'
+        self.bos_id = bos_id
+        self.eos_id = eos_id
+        self.unk_id = unk_id
         self.ngram = kenlm.Model(f_binlm)
         # scale: convert log10 -> loge
         self.register_buffer('scale', torch.tensor(
             10.).log_(), persistent=False)
 
-    def score(self, input_ids: torch.LongTensor, input_lengths: torch.LongTensor):
+    def score(self, input_ids: torch.LongTensor, targets: torch.LongTensor, input_lengths: torch.LongTensor):
+        if not torch.equal(input_ids[:, 1:], targets[:, :-1]):
+            raise RuntimeError(
+                "N-gram model requires that there's no exposure mismatch between input and target.")
+
         # [N, ]
         log_prob = input_ids.new_full(
             input_ids.size()[:1], 0.0, dtype=torch.float)
         for b, (seq, l) in enumerate(zip(input_ids.cpu().tolist(), input_lengths.cpu().tolist())):
-            seq = [str(x) for x in seq[:l]]
-            if seq[0] == '0':
+            seq = [self.vocab[i] for i in seq[:l]]
+            if seq[0] == '</s>':
                 seq[0] = '<s>'
-            seq.append('</s>')
+            seq.append(self.vocab[targets[b][-1].item()])
 
             for t, pb in enumerate(self.ngram.full_scores(' '.join(seq), bos=False, eos=False)):
                 if t == 0:
@@ -386,16 +401,13 @@ class NGram(AbsDecoder):
             input_ids = src_ids
 
         # keep N-1 ids
-        input_ids = input_ids[:, -(self.gram_n-1):]
+        input_ids = input_ids[:, -(self.gram_order-1):]
 
         pred_logp = [[] for _ in range(B)]
         for b, seq in enumerate(input_ids.cpu().tolist()):
-            seq = [str(x) for x in seq]
-            if seq[0] == '0':
-                seq[0] = '<s>'
+            seq = [self.vocab(x) for x in seq]
             state = init_state(self.ngram, seq)
-            # TODO: replace 1 -> <unk>
-            for tok in self.vocab:
+            for tok in self.vocab.values():
                 pred_logp[b].append(update_state(self.ngram, state, tok)[0])
 
         return self.scale*self.scale.new_tensor(pred_logp), input_ids

cat/shared/manager.py
@@ -262,24 +262,34 @@ def train(trainloader, args: argparse.Namespace, manager: Manager):
         with autocast(enabled=enableAMP):
             loss = model(features, labels, input_lengths, label_lengths)/fold
 
-        normalized_loss = loss.detach() * features.size(0)
+        if isinstance(loss, tuple):
+            assert len(loss) == 2
+            loss, norm_size = loss
+        else:
+            norm_size = features.size(0)
+        if not isinstance(norm_size, torch.Tensor):
+            norm_size = input_lengths.new_tensor(
+                int(norm_size), device=args.gpu)
+        else:
+            norm_size = norm_size.to(device=args.gpu, dtype=torch.long)
+
+        normalized_loss = loss.detach() * norm_size
         if 'databalance' in args and args.databalance:
             '''
             get current global size
             efficiently, we can set t_batch_size=args.batch_size, 
             but current impl is more robust
             '''
-            t_batch_size = features.new_tensor(features.size(0))
-            dist.all_reduce(t_batch_size)
-            loss.data = normalized_loss * (world_size / t_batch_size)
+            dist.all_reduce(norm_size)
+            loss.data = normalized_loss * (world_size / norm_size)
         else:
-            t_batch_size = features.size(0) * world_size
+            norm_size *= world_size
 
         scaler.scale(loss).backward()
 
         dist.all_reduce(normalized_loss)
         detach_loss += normalized_loss.float()
-        n_batch += t_batch_size
+        n_batch += norm_size
 
         return detach_loss, n_batch
 
@@ -365,32 +375,6 @@ def train(trainloader, args: argparse.Namespace, manager: Manager):
 
 
 @torch.no_grad()
-def update_bn(trainloader, args: argparse.Namespace, manager: Manager):
-
-    utils.check_parser(args, ['n_steps', 'print_freq',
-                              'rank', 'gpu', 'debug', 'amp'])
-
-    model = manager.model
-    if not hasattr(model.module, 'impl_forward'):
-        raise RuntimeError(
-            "--update-bn requires the model trainer has impl_forward to do pure forward computation.")
-
-    model.train()
-    enableAMP = args.amp
-
-    for i, minibatch in tqdm(enumerate(trainloader), desc=f'Update BN',
-                             unit='batch', total=args.n_steps, disable=(args.gpu != 0), leave=False):
-        # measure data loading time
-        features, input_lengths, labels, label_lengths = minibatch
-        features, labels, input_lengths, label_lengths = features.cuda(
-            args.gpu, non_blocking=True), labels, input_lengths, label_lengths
-
-        with autocast(enabled=enableAMP):
-            _ = model.module.impl_forward(
-                features, labels, input_lengths, label_lengths)
-
-
-@torch.no_grad()
 def evaluate(testloader, args: argparse.Namespace, manager: Manager) -> float:
 
     model = manager.model
@@ -411,13 +395,23 @@ def evaluate(testloader, args: argparse.Namespace, manager: Manager) -> float:
         '''
         loss = model(features, labels, input_lengths, label_lengths)
 
-        real_loss = loss * features.size(0)  # type: torch.Tensor
-        n_batch = real_loss.new_tensor(features.size(0), dtype=torch.long)
+        if isinstance(loss, tuple):
+            assert len(loss) == 2
+            loss, norm_size = loss
+        else:
+            norm_size = features.size(0)
+        if not isinstance(norm_size, torch.Tensor):
+            norm_size = input_lengths.new_tensor(
+                int(norm_size), device=args.gpu)
+        else:
+            norm_size = norm_size.to(device=args.gpu, dtype=torch.long)
+
+        real_loss = loss * norm_size  # type: torch.Tensor
 
         dist.all_reduce(real_loss, dist.ReduceOp.SUM)
-        dist.all_reduce(n_batch, dist.ReduceOp.SUM)
+        dist.all_reduce(norm_size, dist.ReduceOp.SUM)
 
-        cnt_seq += n_batch.item()
+        cnt_seq += norm_size.item()
         total_loss += real_loss.item()
 
     avg_loss = total_loss/cnt_seq

cat/shared/scheduler.py
@@ -55,7 +55,7 @@ def SetupOptim(type_optim: str, paramlist: Iterable[torch.nn.parameter.Parameter
 
 
 class Scheduler(object):
-    def __init__(self, optimizer_configs: dict, paramlist: Iterable[torch.nn.parameter.Parameter], reverse_metric_direc: bool = False):
+    def __init__(self, optimizer_configs: dict, paramlist: Iterable[torch.nn.parameter.Parameter], reverse_metric_direc: bool = True):
         super().__init__()
         if 'type_optim' in optimizer_configs:   # for compablility of previous versions
             optimizer_configs['type'] = optimizer_configs['type_optim']
@@ -134,7 +134,7 @@ class SchedulerEarlyStop(Scheduler):
             lr_stop: float = 1e-5,
             num_ahead: int = 1,
             gamma: float = 0.1,
-            reverse_metric_direc: bool = False):
+            reverse_metric_direc: bool = True):
         super().__init__(optimizer_configs, paramlist, reverse_metric_direc)
         self.lr_stop = lr_stop
         self.epoch_min = epoch_min
@@ -177,7 +177,7 @@ class SchedulerFixedStop(Scheduler):
             optimizer_configs,
             paramlist: Iterable[torch.nn.parameter.Parameter],
             epoch_max: int,
-            reverse_metric_direc: bool = False):
+            reverse_metric_direc: bool = True):
         super().__init__(optimizer_configs, paramlist, reverse_metric_direc)
         self.epoch_max = epoch_max
 
@@ -217,7 +217,7 @@ class SchedulerWarmupMileStone(SchedulerEarlyStop):
             lr_stop: float = 1e-5,
             num_ahead: int = 1,
             gamma: float = 0.1,
-            reverse_metric_direc: bool = False):
+            reverse_metric_direc: bool = True):
         super().__init__(optimizer_configs, paramlist, 0, lr_stop,
                          num_ahead, gamma, reverse_metric_direc)
         if refer_lr == 0.:
@@ -266,7 +266,7 @@ class SchedulerTransformer(SchedulerFixedStop):
             warmup_steps: int,
             epoch_max: int,
             peak_factor: float = 1.0,
-            reverse_metric_direc: bool = False):
+            reverse_metric_direc: bool = True):
         super().__init__(optimizer_configs, paramlist, epoch_max, reverse_metric_direc)
         assert d_model > 0
         assert warmup_steps > 0
@@ -302,7 +302,7 @@ class SchedulerTransformerEarlyStop(SchedulerEarlyStop):
             lr_stop: float = 1e-5,
             num_ahead: int = 1,
             gamma: float = 0.1,
-            reverse_metric_direc: bool = False):
+            reverse_metric_direc: bool = True):
         super().__init__(optimizer_configs, paramlist, 0,
                          lr_stop, num_ahead, gamma, reverse_metric_direc)
         assert d_model > 0
@@ -343,7 +343,7 @@ class SchedulerIterAnnealing(SchedulerFixedStop):
             paramlist: Iterable[torch.nn.parameter.Parameter],
             decay_factor: float,
             epoch_max: int,
-            reverse_metric_direc: bool = False):
+            reverse_metric_direc: bool = True):
         super().__init__(optimizer_configs, paramlist, epoch_max, reverse_metric_direc)
         assert decay_factor > 0. and decay_factor < 1. and epoch_max > 0
         self.decay = decay_factor
@@ -362,7 +362,7 @@ class SchedulerCosineAnnealing(SchedulerFixedStop):
             epoch_max: int,
             period: int = 0,
             decay_factor: float = 1.,
-            reverse_metric_direc: bool = False):
+            reverse_metric_direc: bool = True):
         super().__init__(optimizer_configs, paramlist, epoch_max, reverse_metric_direc)
         assert period >= 0 and lr_min >= 0 and epoch_max > 0
         assert decay_factor > 0. and decay_factor <= 1.