f4f6c22d32d01baaa3ac30e59273089980189e55
maxwellzh <maxwellzh@outlook.com>
2022-09-20 01:23:25 +0800

    [recipe][commonvoice] exclude dev&text from validated one

--- egs/commonvoice/local/data.sh
+++ egs/commonvoice/local/data.sh
@@ -12,9 +12,6 @@ set -e -u
 ("src", type=str, help="Source data folder containing the audios and transcripts.")
 ("-sp", type=float, nargs='*', default=None,
     help="Speed perturbation factor(s). Default: None.")
-("-subsets-fbank", type=str, nargs="+", choices=["train", "validated", "dev", "test"],
-    default=["validated", "dev", "test"], 
-    help="Subset(s) for extracting FBanks. Default: all except 'train'.")
 PARSER
 eval $(python utils/parseopt.py $0 $*)
 
@@ -23,10 +20,14 @@ opt_sp="1.0"
 
 # Extract meta info
 mkdir -p data/src
-for s in $subsets_fbank; do
+for s in dev test train validated; do
     d_set="data/src/$s"
     mkdir -p $d_set
     file="$src/$s.tsv"
+    [ ! -f $file ] && {
+        echo "No such file $file"
+        exit 1
+    }
     cut <$file -f 2 | tail -n +2 | xargs basename -s ".mp3" >$d_set/uid.tmp
     cut <$file -f 2 | tail -n +2 | awk -v path="$src/clips" '{print path"/"$1}' >$d_set/path.tmp
     paste $d_set/{uid,path}.tmp | sort -k 1,1 -u >$d_set/wav.scp
@@ -35,9 +36,26 @@ for s in $subsets_fbank; do
     rm -rf $d_set/{uid,text,path}.tmp
 done
 
+# By default, I use validated+train as the real training data
+# ... but we must exclude the dev & test from the validated one.
+d_train="data/src/excluded_train"
+mkdir -p $d_train
+for file in wav.scp text; do
+    cat data/src/{validated,train}/$file |
+        sort -k 1,1 -u >$d_train/$file.tmp
+    for exc_set in dev test; do
+        python utils/data/exclude_corpus.py \
+            $d_train/$file.tmp \
+            --exclude data/src/$exc_set/$file \
+            >$d_train/$file.tmp.tmp
+        mv $d_train/$file.tmp.tmp $d_train/$file.tmp
+    done
+    mv $d_train/$file.tmp $d_train/$file
+done
+
 # Extract 80-dim FBank features
 python local/make_fbank.py data/src \
-    --subset $subsets_fbank \
+    --subset dev test excluded_train \
     --speed-perturbation $opt_sp ||
     exit 1
 
--- egs/commonvoice/local/make_fbank.py
+++ egs/commonvoice/local/make_fbank.py
@@ -9,41 +9,30 @@ import argparse
 from typing import List, Dict, Any, Tuple
 
 
-prepare_sets = [
-    'train',
-    'validated',
-    'dev',
-    'test'
-]
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("src_data", type=str, default="data/src", nargs='?',
                         help="Directory includes the meta infos.")
-    parser.add_argument("--subset", type=str, nargs='*',
-                        choices=prepare_sets, help=f"Specify datasets in {prepare_sets}")
+    parser.add_argument("--subset", type=str, required=True, nargs='*',
+                        help=f"Subse(s) to be processed.")
     parser.add_argument("--cmvn", action="store_true", default=False,
                         help="Apply CMVN by utterance, default: False.")
     parser.add_argument("--speed-perturbation", type=float, dest='sp',
-                        nargs='*', default=[], help=f"Add speed perturbation to subset: {', '.join(prepare_sets)}")
+                        nargs='*', default=[], help=f"Add speed perturbation to subset(s).")
     args = parser.parse_args()
 
     assert os.path.isdir(args.src_data)
-    if args.subset is not None:
-        for _set in args.subset:
-            assert _set in prepare_sets, f"--subset {_set} not in predefined datasets: {prepare_sets}"
-        prepare_sets = args.subset
+    subsets_ = args.subset
 
     for _sp_factor in args.sp:
         assert (isinstance(_sp_factor, float) or isinstance(_sp_factor, int)) and _sp_factor > 0, \
             f"Unsupport speed pertubation value: {_sp_factor}"
 
-    d_sets = [os.path.join(args.src_data, _set) for _set in prepare_sets]
+    d_sets = [os.path.join(args.src_data, _set) for _set in subsets_]
 
     from cat.utils.data import data_prep
     data_prep.prepare_kaldi_feat(
-        subsets=prepare_sets,
+        subsets=subsets_,
         trans=[f"{path}/text" for path in d_sets],
         audios=[f"{path}/wav.scp" for path in d_sets],
         num_mel_bins=80,
